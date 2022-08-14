424e0952146018762d1c23e4c984743f38641e8d
dalma-ponce <p.d.f.1915@gmail.com>
2022-08-14 12:45:34 -0300

    Ejercicio valoracion final, parte python

---
+++ ejercicio_2.py
@@ -0,0 +1,9 @@
+''' Crear una lista en Python denominada “Dueno2” y recorrerla con un bucle mostrando sus elementos por
+pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
+23546987, “Maria”, “Perez”, 4789689, “Pueyrredon 811” que representan los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección) '''
+
+Dueno2 = [23546987, "Maria", "Perez", 4789689, "Pueyrredon 811"]
+
+for i in Dueno2:
+    if i != 23546987 and i != "Perez":
+        print(i)  
