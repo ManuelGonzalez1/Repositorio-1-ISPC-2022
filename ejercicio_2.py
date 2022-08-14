21137e4ed37b0317d1ff8e2ba26b45e15ccd2914
Dalma Ponce <106714820+dalma-ponce@users.noreply.github.com>
2022-08-14 17:00:21 -0300

    Update ejercicio_2.py

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -2,8 +2,10 @@
 pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
 23546987, “Maria”, “Perez”, 4789689, “Pueyrredon 811” que representan los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección) '''
 
+''' creo lista llamada "Dueno2" '''
 Dueno2 = [23546987, "Maria", "Perez", 4789689, "Pueyrredon 811"]
 
+''' Recorro la lista con un ciclo for, usando un condicional para evitar mostrar el apellido y el DNI '''
 for i in Dueno2:
     if i != 23546987 and i != "Perez":
         print(i)  
